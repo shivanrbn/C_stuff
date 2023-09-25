41a0bc0a2e7473be11d14ed0378e3b275d87f64d
Shivan Rambaran <shivanrbn@hotmail.com>
2023-09-25 19:32:37 +0200

    initial commit, return_2 stuff, updated readme

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # C_stuff
 stuff about C, someday there will be a compiler in here...
+
+
+## Writing a compiler tutorial
+the following tutorial is taken from: https://norasandler.com/2017/11/29/Write-a-Compiler.html 
+
+
+
+
+
+```
+
+```
---
+++ return_2.s
@@ -0,0 +1,12 @@
+	.file	"return_2.c"
+	.text
+	.section	.text.startup,"ax",@progbits
+	.p2align 4
+	.globl	main
+	.type	main, @function
+main:
+	movl	$2, %eax
+	ret
+	.size	main, .-main
+	.ident	"GCC: (GNU) 13.2.1 20230801"
+	.section	.note.GNU-stack,"",@progbits
